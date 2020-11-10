28e904215d179462983de3f737e872a5b19b0a9c
Bill Watters <billywatters13@gmail.com>
2020-11-10 18:26:58 +0000

    Add second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+
+end
+
+
+@store1 = Store.find(1) 
+@store2 = Store.find(2) 
+@store1.update(name: "Good Store")
