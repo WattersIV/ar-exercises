0d685987086b851a04546fa0d695365f9463d038
Bill Watters <billywatters13@gmail.com>
2020-11-10 19:55:03 +0000

    Add seventh exercise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -17,10 +17,10 @@ class Employee < ActiveRecord::Base
 end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60) 
-@store1.employees.create(first_name: "Bill", last_name: "Watters", hourly_rate: 600) 
+@store1.employees.create(first_name: "Bill", last_name: "Watters", hourly_rate: 800) 
 @store1.employees.create(first_name: "Salim", last_name: "Dream", hourly_rate: 80) 
 @store2.employees.create(first_name: "Meg", last_name: "Vanilli", hourly_rate: 40) 
-@store2.employees.create(first_name: "Sam", last_name: "Art", hourly_rate: 550) 
+@store2.employees.create(first_name: "Sam", last_name: "Art", hourly_rate: 200) 
 
 puts @store1.employees.count() #3
 puts @store2.employees.count() #2
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3} 
+  validates :annual_revenue, numericality: {only_interger: true, greater_than: 0}
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store_id, presence: true 
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+end
+
+puts "Store name:"
+@user_store_name = gets.chomp 
+store = Store.create(name: "#{@user_store_name}") 
+# puts store.errors.messages
